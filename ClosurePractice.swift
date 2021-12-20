d3ed2ce22f738efe60d2df785560204516287a25
Ahmad MacBook <ahmad@Ahmads-MacBook-Pro.local>
2021-12-20 19:43:03 +0300

    Closure Practice V2

--- ClosurePractice.swift
+++ ClosurePractice.swift
@@ -4,13 +4,13 @@ struct Book {
     let title : String
     let desc : String
     let auth : String
-    let radingAge : Int
+    let readingAge : Int
      
 }
 
-let book1 = Book.init(title: "Swift", desc: "iOS", auth: "Ahmad", radingAge: 17)
-let book2 = Book.init(title: "Kotlin", desc: "Andriod", auth: "Mohammed", radingAge: 18)
-let book3 = Book.init(title: "Cyber", desc: "Security", auth: "Huda", radingAge: 20)
+let book1 = Book.init(title: "Swift", desc: "iOS", auth: "Ahmad", readingAge: 17)
+let book2 = Book.init(title: "Kotlin", desc: "Andriod", auth: "Mohammed", readingAge: 18)
+let book3 = Book.init(title: "Cyber", desc: "Security", auth: "Huda", readingAge: 20)
 
 let books = [ book1 , book2 , book3 ]
 
