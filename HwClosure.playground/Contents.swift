0ac0e001618173ad953338b99017b4861ec9bf1a
Najla Talal <Najla.Talal.ruh.s@tuwaiq.edu.sa>
2021-12-20 21:04:17 +0300

    Najla Alharbi

---
+++ HwClosure.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+struct Book{
+    var title: String
+    var auther: String
+    var radingAge: Int
+    var pageCount: Int
+}
+
+let book1 = Book.init(title: "ABCD", auther: "Ali", radingAge: 14, pageCount: 200)
+let book2 = Book.init(title: "how to", auther: "Khalid", radingAge: 18, pageCount: 120)
+let book3 = Book.init(title: "intro to swift", auther: "Huda", radingAge: 8, pageCount: 90)
+
+let allBooks = [ book1 , book2 , book3 ]
+
+let sortedBooks = allBooks.sorted{$0.radingAge <= $1.radingAge }
+
+let name = allBooks.filter ({ return $0.auther == "Huda"})
+
+
