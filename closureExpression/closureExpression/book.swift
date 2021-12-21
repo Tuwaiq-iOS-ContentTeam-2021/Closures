122e3682d0f0bca3549e32888d7cd9012a3e97b1
Areejalharbi57 <areej.mohammad.ruh.s@tuwaiq.edu.sa>
2021-12-21 08:40:52 +0300

    add my project

---
+++ closureExpression/closureExpression/book.swift
@@ -0,0 +1,14 @@
+//
+//  book.swift
+//  closureExpression
+//
+//  Created by Areej on 21/12/2021.
+//
+
+import Foundation
+struct Book{
+    var title: String
+    var auther: String
+    var radingAge: Int
+    var pageCount: Int
+}
