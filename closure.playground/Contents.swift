2032ee2518dbf675fa2015d08d9ddf198152a549
SahabMAlharbi <Sahab.Mubarak.ruh.s@tuwaiq.edu.sa>
2021-12-21 08:54:11 +0300

    sahab

---
+++ closure.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+struct Book{
+    var title: String
+    var auther: String
+    var radingAge: Int
+    var pageCount: Int
+}
+
+let book1 = Book.init(title: "ABCD", auther: "Ali", radingAge: 14, pageCount: 200)
+let book2 = Book.init(title: "how to", auther: "Khalid", radingAge: 18, pageCount: 120)
+let book3 = Book.init(title: "intro to swift", auther: "Huda", radingAge: 8, pageCount: 90)
+
+let allBooks = [ book1 , book2 , book3 ]
+
+let finding = allBooks.filter { $0.auther == "Huda"}
+
+print(finding)
